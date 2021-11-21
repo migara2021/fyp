fe714b9e66093f879e56650f7f9df26ded9dd76e
migara2021 <94514000+migara2021@users.noreply.github.com>
2021-11-22 03:56:01 +0530

    Update entities_list.py

--- utils/entities_list.py
+++ utils/entities_list.py
@@ -1,21 +1,8 @@
-# -*- coding: utf-8 -*-
-# @Author: Wenwen Yu
-# @Created Time: 7/8/2020 9:34 PM
 
 Entities_list = [
-    "company",
-    "address",
-    "date",
-    "total"
+    "ItemDescription",
+    "ItemTotal",
+    "ItemUnitCount",
+    "ItemUnitValue"
 ]
 
-# Entities_list = [
-#     "ticket_num",
-#     "starting_station",
-#     "train_num",
-#     "destination_station",
-#     "date",
-#     "ticket_rates",
-#     "seat_category",
-#     "name"
-# ]
